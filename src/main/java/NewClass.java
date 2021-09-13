72ce45a6f7999871bc9ed0ead8e815d0c97508ee
notmac1853 <90333994+notmac1853@users.noreply.github.com>
2021-09-13 17:11:05 -0400

    Update NewClass.java

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,13 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Marco
  */
 public class NewClass {
-    
+    public static void main(String[] args){
+        for(int i=0; i <= 100; i++){ //This for loop iterates through all numbers from 0 to 100 (inclusive)
+            if (i%2 == 0){ //Checks to see if the remainder of the division between i and 2 is 0. If the remainder is 0, it is an even number.
+                System.out.println(i); //If the if statement is true, then i is printed on a new line
+                //the loop then repeats
+            }
+        }
+        
+    }
 }
